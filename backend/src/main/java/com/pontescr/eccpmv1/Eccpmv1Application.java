95ba46dc268c8d08b2d85a8b7bc97ee195ba46d3
Cristiano Pontes <pontescr@gmail.com>
2021-05-19 20:13:04 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/pontescr/eccpmv1/Eccpmv1Application.java
@@ -0,0 +1,13 @@
+package com.pontescr.eccpmv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eccpmv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eccpmv1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pontescr/eccpmv1/Eccpmv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontescr.eccpmv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eccpmv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
