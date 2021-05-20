9c2de1bbf97f60e19790d1e831f7de95cafb7442
Cristiano Pontes <pontescr@gmail.com>
2021-05-19 23:00:04 -0300

    Seed

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,50 @@
+INSERT INTO tb_team (name) VALUES ('SECRETARIA');
+INSERT INTO tb_team (name) VALUES ('CANTINA');
+INSERT INTO tb_team (name) VALUES ('CONTATO');
+INSERT INTO tb_team (name) VALUES ('DIRIGENTES');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986', '15/11/1981', '15/03/2008', 0, 'Larissa Damasceno', 'Cristiano Pontes', 'Autônoma', 'Informática', '996853842', '996083469');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986', '15/11/1981', '15/03/2008', 0, 'Nathane Magalhães', 'Pedro Amorim', 'Autônoma', 'Informática', '996853842', '996083469');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986', '15/11/1981', '15/03/2008', 0, 'Isabella Lisboa', 'Thiago Rodrigues', 'Autônoma', 'Informática', '996853842', '996083469');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986','','', 1, 'Francilene Pontes','', 'Autônoma','', '996853842','');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986', '15/11/1981', '15/03/2008', 0, 'Nice Paiva', 'Antônio Antão', 'Autônoma', 'Informática', '996853842', '996083469');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986', '15/11/1981', '15/03/2008', 0, 'Maria Joice', 'Cássio Gregório', 'Autônoma', 'Informática', '996853842', '996083469');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986', '15/11/1981', '15/03/2008', 0, 'Cleudes Costa', 'Cleudimar Rubens', 'Autônoma', 'Informática', '996853842', '996083469');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986', '15/11/1981', '15/03/2008', 0, 'Elizete Pereira', 'Adimilson Pereira', 'Autônoma', 'Informática', '996853842', '996083469');
+
+INSERT INTO tb_servants (address, birth_her_or_not_married, birth_him, birth_wedding, married_status, name_her_or_not_married,
+name_him, occupation_her_or_not_married, occupation_him, phone_her_or_not_married, phone_him) VALUES ('Avenida Parque Atheneu', 
+'25/05/1986', '15/11/1981', '15/03/2008', 0, 'Rejanne Maria', 'Divino Cardoso', 'Autônoma', 'Informática', '996853842', '996083469');
+
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (1, 1);
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (1, 2);
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (1, 3);
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (1, 4);
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (2, 5);
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (2, 6);
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (2, 7);
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (2, 8);
+INSERT INTO tb_team_servants (team_id, servants_id) VALUES (3, 9);
